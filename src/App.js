6f2639558aa1b202b1544af13cfbde35c4d8c304
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2023-08-19 18:43:17 +0530

    1

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Flashcard from './components/Flashcard';
 
 const App = () => {
   const flashcards = [
-    { question: 'What is the capital of France?', answer: 'Paris' },
-    { question: 'What is 2 + 2?', answer: '4' },
+    { question: 'A light rope is wound around a hollow cylinder of mass 5 kg and radius 70 cm. The rope is pulled with a force of 52.5 N. The angular acceleration of the cylinder will be _________ rad s-2.', answer: '15' },
+    { question: 'For a rolling spherical shell, the ratio of rotational kinetic energy and total kinetic energy is x. The value of x/5 is?', answer: '2' },
+    { question: 'For a rolling spherical shell, the ratio of rotational kinetic energy and total kinetic energy is x. The value of x/5 is?', answer: '2' },
+    { question: 'For a rolling spherical shell, the ratio of rotational kinetic energy and total kinetic energy is x. The value of x/5 is?', answer: '2' },
     
   ];
 
   return (
     <div className="app">
-      <h1>Flashcards Website</h1>
+      <h1 className='text-center'>Learn for JEE using Flashcards!!</h1>
+      
       <div className="flashcard-container">
         {flashcards.map((flashcard, index) => (
           <Flashcard
