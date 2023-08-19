26493b60f22485d392c60824ab7ad9c47635a855
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2023-08-19 18:32:05 +0530

    created new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './components/flashcard.css'
+import Flashcard from './components/Flashcard';
 
-function App() {
+const App = () => {
+  const flashcards = [
+    { question: 'What is the capital of France?', answer: 'Paris' },
+    { question: 'What is 2 + 2?', answer: '4' },
+    
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Flashcards Website</h1>
+      <div className="flashcard-container">
+        {flashcards.map((flashcard, index) => (
+          <Flashcard
+            key={index}
+            question={flashcard.question}
+            answer={flashcard.answer}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Flashcard.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const Flashcard = ({ question, answer }) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const toggleAnswer = () => {
+    setShowAnswer(!showAnswer);
+  };
+
+  return (
+    <div className="flashcard" onClick={toggleAnswer}>
+      <div className={`flashcard-inner ${showAnswer ? 'flipped' : ''}`}>
+        <div className="flashcard-front">{question}</div>
+        <div className="flashcard-back">{answer}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Flashcard;
