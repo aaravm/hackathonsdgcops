4e85c849d81313ff0f219abc42cbb0e4487eb143
aaravm <32593731+aaravm@users.noreply.github.com>
2023-08-19 20:30:16 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './navbar.css'; // Import the corresponding CSS file
 
-const Navbar = () => {
-    const scrollToMCQ = () => {
-        const mcqSection = document.getElementById('mcq-section');
-        mcqSection.scrollIntoView({ behavior: 'smooth' });
-      };
-    
+const Navbar = ({ flash, mc }) => {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Learn For JEE in an Interactive way! &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="#"> Flashcards </a> &nbsp; &nbsp;  <a href="#"> MCQ's </a> </div>
-      {/* <div className="navbar-brand">Flashcards  &nbsp; &nbsp;  MCQ's  </div> */}
+      <div className="navbar-brand">
+        Learn For JEE in an Interactive way! &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <a href={flash}>Flashcards</a> &nbsp; &nbsp; <a href={mc}>MCQ's</a>
+      </div>
       <div className="navbar-brand"></div>
     </nav>
   );
